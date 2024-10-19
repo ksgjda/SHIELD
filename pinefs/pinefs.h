079ab4bd3f75baaf10ce3266d579d0ef9255a4aa
ksgjda <shkld@proton.me>
2024-10-18 18:31:50 -0700

    Add PineFS codebase

---
+++ pinefs/pinefs.h
@@ -0,0 +1,164 @@
+#pragma once
+#include <openssl/evp.h>
+
+#include <string>
+
+#include "rocksdb/env_encryption.h"
+
+namespace ROCKSDB_NAMESPACE {
+class AESCTRCipherStream;
+
+using evp_ctx_unique_ptr =
+    std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)>;
+
+// The encryption method supported.
+enum class EncryptionMethod : int {
+  kUnknown = 0,
+  kAES128_CTR = 1,
+  kAES192_CTR = 2,
+  kAES256_CTR = 3,
+  // OpenSSL support SM4 after 1.1.1 release version.
+  kSM4_CTR = 4,
+};
+
+static const std::unordered_map<std::string, EncryptionMethod>
+    encryption_method_enum_map = {{"", EncryptionMethod::kUnknown},
+                                  {"AES128CTR", EncryptionMethod::kAES128_CTR},
+                                  {"AES192CTR", EncryptionMethod::kAES192_CTR},
+                                  {"AES256CTR", EncryptionMethod::kAES256_CTR},
+#if OPENSSL_VERSION_NUMBER > 0x1010100fL && !defined(OPENSSL_NO_SM4)
+                                  {"SM4CTR", EncryptionMethod::kSM4_CTR}
+#endif
+};
+
+// Get the key size of the encryption method.
+size_t KeySize(EncryptionMethod method);
+
+// Get the block size of the encryption method.
+size_t BlockSize(EncryptionMethod method);
+
+// Get the OpenSSL EVP_CIPHER according to the encryption method.
+const EVP_CIPHER* GetEVPCipher(EncryptionMethod method);
+
+// Get the last OpenSSL error message.
+std::string GetOpenSSLErrors();
+
+// Convert an OpenSSL error to an IOError Status.
+#define OPENSSL_RET_NOT_OK(call, msg)                  \
+  if (UNLIKELY((call) <= 0)) {                         \
+    return Status::IOError((msg), GetOpenSSLErrors()); \
+  }
+
+Status NewAESCTRCipherStream(EncryptionMethod method,
+                             const std::string& file_key,
+                             const std::string& file_key_iv,
+                             std::unique_ptr<AESCTRCipherStream>* result);
+
+// The cipher stream for AES-CTR encryption.
+class AESCTRCipherStream : public BlockAccessCipherStream {
+ public:
+  AESCTRCipherStream(const EncryptionMethod method, std::string file_key,
+                     uint64_t iv_high, uint64_t iv_low)
+      : method_(method),
+        file_key_(std::move(file_key)),
+        initial_iv_high_(iv_high),
+        initial_iv_low_(iv_low) {}
+
+  ~AESCTRCipherStream() override = default;
+
+  size_t BlockSize() override;
+
+  enum class EncryptType : int { kDecrypt = 0, kEncrypt = 1 };
+
+  Status Encrypt(uint64_t file_offset, char* data, size_t data_size) override {
+    return Cipher(file_offset, data, data_size, EncryptType::kEncrypt);
+  }
+
+  Status Decrypt(uint64_t file_offset, char* data, size_t data_size) override {
+    return Cipher(file_offset, data, data_size, EncryptType::kDecrypt);
+  }
+
+ protected:
+  // Following methods required by BlockAccessCipherStream is unused.
+
+  void AllocateScratch(std::string& /*scratch*/) override {
+    // should not be called.
+    assert(false);
+  }
+
+  Status EncryptBlock(uint64_t /*block_index*/, char* /*data*/,
+                      char* /*scratch*/) override {
+    return Status::NotSupported("EncryptBlock should not be called.");
+  }
+
+  Status DecryptBlock(uint64_t /*block_index*/, char* /*data*/,
+                      char* /*scratch*/) override {
+    return Status::NotSupported("DecryptBlock should not be called.");
+  }
+
+ private:
+  Status Cipher(uint64_t file_offset, char* data, size_t data_size,
+                EncryptType encrypt_type);
+
+  const EncryptionMethod method_;
+  const std::string file_key_;
+  const uint64_t initial_iv_high_;
+  const uint64_t initial_iv_low_;
+};
+
+struct AESEncryptionOptions {
+  std::string instance_key;
+  EncryptionMethod method;
+  AESEncryptionOptions(std::string k = "",
+                       EncryptionMethod m = EncryptionMethod::kUnknown)
+      : instance_key(std::move(k)), method(m) {
+    assert(instance_key.size() == KeySize(method));
+  }
+};
+
+class AESEncryptionProvider : public EncryptionProvider {
+ public:
+  AESEncryptionProvider();
+  ~AESEncryptionProvider() override = default;
+
+  static const char* kClassName() { return "AES"; }
+  const char* Name() const override { return kClassName(); }
+  bool IsInstanceOf(const std::string& name) const override;
+
+  size_t GetPrefixLength() const override { return kDefaultPageSize; }
+
+  Status CreateNewPrefix(const std::string& /*fname*/, char* prefix,
+                         size_t prefix_length) const override;
+
+  Status AddCipher(const std::string& /*descriptor*/, const char* /*cipher*/,
+                   size_t /*len*/, bool /*for_write*/) override {
+    return Status::NotSupported();
+  }
+
+  Status CreateCipherStream(
+      const std::string& fname, const EnvOptions& options, Slice& prefix,
+      std::unique_ptr<BlockAccessCipherStream>* result) override;
+
+ private:
+  friend class AESEncryptionProviderDeterministicTest_DecryptFileKey_Test;
+  friend class AESEncryptionProviderDeterministicTest_EncryptFileKey_Test;
+  friend class AESEncryptionProviderTest_EncryptAndDecryptFileKey_Test;
+  friend class AESEncryptionProviderTest_WriteAndReadEncryptionHeader_Test;
+
+  struct FileEncryptionInfo {
+    EncryptionMethod method = EncryptionMethod::kUnknown;
+    std::string key;
+    std::string iv;
+  };
+
+  Status WriteEncryptionHeader(char* header_buf) const;
+  Status ReadEncryptionHeader(Slice prefix,
+                              FileEncryptionInfo* file_info) const;
+
+  Status EncryptFileKey(char* file_key, size_t file_key_size) const;
+  Status DecryptFileKey(char* file_key, size_t file_key_size) const;
+
+  AESEncryptionOptions aes_options_;
+};
+
+}  // namespace ROCKSDB_NAMESPACE
---
+++ pinefs/pinefs.mk
@@ -0,0 +1,3 @@
+pinefs_SOURCES = pinefs.cc
+pinefs_HEADERS = pinefs.h
+pinefs_LDFLAGS = -lcrypto -u pinefs_reg
